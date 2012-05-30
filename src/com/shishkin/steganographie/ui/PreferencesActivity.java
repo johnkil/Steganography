9aa07af9bc21d4a80f9c4201fcf69199794c36d1
johnkil <johnkil78@gmail.com>
2012-05-30 19:10:40 +0400

    minor changes
    - change app name
    - add home button for action bar in PreferencesActivity

--- src/com/shishkin/steganographie/ui/PreferencesActivity.java
+++ src/com/shishkin/steganographie/ui/PreferencesActivity.java
@@ -1,8 +1,11 @@
 package com.shishkin.steganographie.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.actionbarsherlock.app.SherlockPreferenceActivity;
+import com.actionbarsherlock.view.MenuItem;
 import com.shishkin.steganographie.R;
 
 /**
@@ -12,12 +15,31 @@ import com.shishkin.steganographie.R;
  *
  */
 public class PreferencesActivity extends SherlockPreferenceActivity {
+	private static final String LOG_TAG = PreferencesActivity.class.getSimpleName();
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		Log.v(LOG_TAG, "onCreate() called");
 		super.onCreate(savedInstanceState);
 		// inflate data from xml
 		addPreferencesFromResource(R.xml.preferences);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		Log.v(LOG_TAG, "onOptionsItemSelected() called");
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			Intent homeIntent = new Intent(this, MainActivity.class);
+			homeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			startActivity(homeIntent);
+			break;
+
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+		return true;
 	}
 
 }
